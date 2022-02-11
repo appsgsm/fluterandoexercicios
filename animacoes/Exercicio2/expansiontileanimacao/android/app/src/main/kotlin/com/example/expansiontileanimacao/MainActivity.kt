667f81010cf42aa8cd5c3bdea717fb5abe42897b
Gisele <gisetoledo83@outlook.com>
2022-02-11 16:17:32 -0300

    Exercicio 2 Expanded Tile com animacao

---
+++ animacoes/Exercicio2/expansiontileanimacao/android/app/src/main/kotlin/com/example/expansiontileanimacao/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.expansiontileanimacao
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ animacoes/Exercicio2/expansiontileanimacao/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:mysample/CustomTile.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Animando como Expanded List';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: ListView(
+        children: List.generate(
+            10,
+            (index) => CustomTile(
+                  texto: 'My expansion tile ' + index.toString(),
+                )),
+      ),
+    );
+  }
+}
