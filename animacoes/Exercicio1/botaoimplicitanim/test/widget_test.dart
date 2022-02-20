51fb1eeb16a24783aef68f8cabefa6a5944e461a
Gisele <gisetoledo83@outlook.com>
2022-02-20 12:17:55 -0300

    Comentando os testes

--- animacoes/Exercicio1/botaoimplicitanim/test/widget_test.dart
+++ animacoes/Exercicio1/botaoimplicitanim/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:botaoimplicitanim/main.dart';
+//import 'package:botaoimplicitanim/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
