0281b051eacf9a921848079471cfc3ad819a3d1a
Gisele <gisetoledo83@outlook.com>
2022-03-07 17:15:14 -0300

    Store e testes

--- todoapp/lib/json_placeholder/store/todo_store.dart
+++ todoapp/lib/json_placeholder/store/todo_store.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/cupertino.dart';
-
 import '../model/todo_model.dart';
+import '../services/todo_service.dart';
 
 class TodoStore extends ChangeNotifier {
-  final List<TodoModel> todos = [];
+  final _todoService = TodoService();
 
-  Future<List<TodoModel>> getAllTodos() async {
-    return [];
+  List<TodoModel> _todos = [];
+  List<TodoModel> get todos => List<TodoModel>.unmodifiable(_todos);
+
+  Future<void> getAllTodos() async {
+    _todos = await _todoService.getAllTodos();
+    notifyListeners();
   }
 }
--- todoapp/test/json_placeholder/store/todo_store_test.dart
+++ todoapp/test/json_placeholder/store/todo_store_test.dart
@@ -1,8 +1,9 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:todoapp/json_placeholder/store/todo_store.dart';
 
 void main() async {
   final store = TodoStore();
 
   await store.getAllTodos();
+
+  print(store.todos[0].title);
 }
