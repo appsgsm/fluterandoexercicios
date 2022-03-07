ef7ff8cfacc586292c93e0c9cde161f0f60ec575
Gisele <gisetoledo83@outlook.com>
2022-03-07 17:14:57 -0300

    Model

--- todoapp/lib/json_placeholder/model/todo_model.dart
+++ todoapp/lib/json_placeholder/model/todo_model.dart
@@ -3,5 +3,10 @@ class TodoModel {
   final String title;
   final bool isChecked;
 
-  TodoModel(this.id, this.title, this.isChecked);
+  TodoModel({required this.id, required this.title, required this.isChecked});
+
+  factory TodoModel.fromJson(dynamic json) {
+    return TodoModel(
+        id: json['id'], title: json['title'], isChecked: json['completed']);
+  }
 }
